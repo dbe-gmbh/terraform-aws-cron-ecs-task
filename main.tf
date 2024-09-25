be65434aec411949574d517082aef3f99948b60b
Stefan Pokorny <spy@dbe.gmbh>
2024-09-25 17:47:49 +0200

    modules for single web app and shibboleth for meanwhile retired bearingpoint poc

--- main.tf
+++ main.tf
@@ -8,23 +8,19 @@ resource "aws_ecs_task_definition" "main" {
   cpu                      = var.cpu_unit
   memory                   = var.memory
 
-  container_definitions = data.template_file.main.rendered
-}
-
-data "template_file" "main" {
-  template = file("${path.module}/task_definition_${var.use_cloudwatch_logs ? "cloudwatch" : "elasticsearch"}.json")
-
-  vars = {
-    ecr_image_url  = var.repo_url
-    name           = var.app
-    name_index_log = lower(var.app)
-    listen_port    = var.listen_port
-    region         = var.region
-    environment    = jsonencode(concat(local.main_environment, var.environment_list))
-    prefix_logs    = var.prefix_logs
-    port           = var.listen_port
-    es_url         = var.es_url
-  }
+  container_definitions = templatefile("${path.module}/task_definition_${var.use_cloudwatch_logs ? "cloudwatch" : "elasticsearch"}.json",
+    {
+      ecr_image_url  = var.repo_url
+      name           = var.app
+      name_index_log = lower(var.app)
+      listen_port    = var.listen_port
+      region         = var.region
+      environment    = jsonencode(concat(local.main_environment, var.environment_list))
+      prefix_logs    = var.prefix_logs
+      port           = var.listen_port
+      es_url         = var.es_url
+    }
+  )
 }
 
 locals {
